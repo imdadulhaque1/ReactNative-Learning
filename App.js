c35f634b2da4fbc0015717d8cf16cd1299c854b6
Imdadul Haque <45633928+imdadulhaque1@users.noreply.github.com>
2022-12-31 15:57:13 +0600

    Array to List using Map()

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import React from 'react';
 import ViewTextFlexbox from './src/ViewTextFlexbox';
 import IncreDecrements from './src/IncreDecrements';
 import InputField from './src/InputField';
+import ArrayToList from './src/ArrayToList';
 
 const App = () => {
   return (
     <>
-      <InputField />
+      <ArrayToList />
     </>
   );
 };
---
+++ src/ArrayToList.js
@@ -0,0 +1,68 @@
+/* eslint-disable prettier/prettier */
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
+
+const ArrayToList = () => {
+  const [data, setData] = useState([
+    {id: 1, name: 'Imdadul Haque'},
+    {id: 2, name: 'Israt Jahan Maisha'},
+    {id: 3, name: 'Enamul Haque'},
+    {id: 4, name: 'Shampa Khatun'},
+    {id: 5, name: 'Maisha'},
+    {id: 6, name: 'Imdadul Haque Imdad'},
+    {id: 7, name: 'Maisha'},
+    {id: 8, name: 'Imdadul Haque'},
+  ]);
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        {data.map((dataList, index) => {
+          return (
+            <View style={styles.ListViewStyle} key={dataList.id}>
+              <TouchableOpacity style={styles.ListStyle}>
+                <Text style={styles.idStyle}>{dataList.id}</Text>
+                <Text style={styles.nameStyle}>{dataList.name}</Text>
+              </TouchableOpacity>
+            </View>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default ArrayToList;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  ListViewStyle: {
+    width: '100%',
+  },
+  ListStyle: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    fontSize: 20,
+    backgroundColor: '#4e5485',
+    margin: 5,
+    paddingVertical: 30,
+  },
+  idStyle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  nameStyle: {
+    fontSize: 17,
+    color: 'white',
+  },
+});
