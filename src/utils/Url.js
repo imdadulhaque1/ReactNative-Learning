a72fdb538e8f2e5e42ec6a9a1b8fdc069f76e350
Imdadul Haque <45633928+imdadulhaque1@users.noreply.github.com>
2023-01-02 15:39:49 +0600

    Fetch Data Done

--- src/utils/Url.js
+++ src/utils/Url.js
@@ -1,3 +1,5 @@
+/* eslint-disable prettier/prettier */
 const albumPhotoUrl = 'https://jsonplaceholder.typicode.com/albums/1/photos';
+const userOnePostUrl = 'https://jsonplaceholder.typicode.com/users/1/posts';
 
-export {albumPhotoUrl};
+export {albumPhotoUrl, userOnePostUrl};
