1a49fb67340bac50ae968e0c26254933eb56e0d8
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2023-02-16 11:59:36 +0600

    react-native-video-controller setup and done with navigate functionality

--- src/screen/InbuiltVideoController.js
+++ src/screen/InbuiltVideoController.js
@@ -1,20 +1,31 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import Video from 'react-native-video';
+import VideoPlayer from 'react-native-video-controls';
+import {useNavigation} from '@react-navigation/native';
 
 const InbuiltVideoController = () => {
+  const navigate = useNavigation();
   return (
     <View style={styles.container}>
       <Text style={styles.videoTitleStyle}>Inbuilt Video Controller</Text>
-      <Video
+      <>
+        <VideoPlayer
+          style={styles.backgroundVideo}
+          source={require('../Assets/videos/Beauty_Nature.mp4')}
+          navigator={navigate}
+        />
+        {/* <Video
         style={styles.backgroundVideo}
         source={require('../Assets/videos/Beauty_Nature.mp4')}
-      />
+      /> */}
+      </>
     </View>
   );
 };
 
 export default InbuiltVideoController;
+
+const {height, width} = Dimensions.get('screen');
 
 const styles = StyleSheet.create({
   container: {
@@ -26,11 +37,18 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginVertical: 5,
   },
+  // videoContainer: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  // },
   backgroundVideo: {
     position: 'absolute',
-    top: 0,
+    top: 100,
     bottom: 0,
     left: 0,
     right: 0,
+    height: '40%',
+    width: width,
   },
 });
